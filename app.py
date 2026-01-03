833935646c520df8daae43a4d1dfc59758cb8d14
HamHocHoi <nguyencongtinh@gmail.com>
2026-01-03 14:57:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
                 - Tích hợp Học thông qua chơi vào 1 số hoạt động phù hợp.
                 - Riêng các trò chơi trong tiến trình dạy học cần TRÌNH BÀY RÕ LUẬT CHƠI.
                 - Không cần ghi "Mục tiêu HTQC".
-                - Mỗi Bài học có thể có nhiều tiết, do đó cần CHIA TIẾT cho hợp lý, ĐÚNG YÊU CẦU CỦA NGƯỜI DÙNG nhập vào ở ô {so_tiet}. Mỗi tiết học chỉ có 42 phút, hãy điều chỉnh lượng kiến thức và hoạt động hợp lý. 
+                - Mỗi Bài học có thể có nhiều tiết, do đó cần CHIA TIẾT cho hợp lý, ĐÚNG YÊU CẦU CỦA NGƯỜI DÙNG nhập vào ở ô "{so_tiet}". Mỗi tiết học chỉ có 42 phút, hãy điều chỉnh lượng kiến thức và hoạt động hợp lý. 
                 - Không kèm chú thích nguồn trong bài soạn.
                 - Tuyệt đối chỉ bao gồm 4 Hoạt động, không phát sinh thêm.
                 - LUÔN LUÔN TUÂN THỦ THEO NHỮNG YÊU CẦU TRÊN
@@ -302,7 +302,7 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
                 Lưu ý chung: Bám sát nội dung trong Sách giáo khoa và sách giáo viên (từ tài liệu đính kèm) để đưa nội dung vào bài soạn cho chính xác. KHÔNG dùng ký tự # ở đầu dòng.
 
-                LƯU Ý QUAN TRỌNG TỪ NGƯỜI DÙNG: {yeu_cau_them}, {so_tiet}, {noidung_bosung}
+                LƯU Ý QUAN TRỌNG TỪ NGƯỜI DÙNG: "{yeu_cau_them}", "{so_tiet}", "{noidung_bosung}"
                 """
 
                 input_data = [prompt_instruction]
